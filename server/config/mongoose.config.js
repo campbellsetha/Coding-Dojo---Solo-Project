50608f1f09423c377d385474228b2bc6bcf74aaa
campbellsetha <71057297+campbellsetha@users.noreply.github.com>
2023-04-18 17:19:57 -0700

    Delete server directory

--- server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/user', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('Established connection with database: user'))
-    .catch((err) => console.log('Something went wrong when trying to connect', err));
